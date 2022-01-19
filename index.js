372659f08f2b13a4ee036bbc62357d0c94732d8f
VMNthemaster <varad.nadkarni09@gmail.com>
2022-01-19 23:17:41 +0530

    another new commit

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(express.json())
 // available routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
-
+app.get('/', (req, res) => {
+  res.json({message: "Set Up Success"})
+})
 app.listen(process.env.PORT || port, () => {
   console.log(`iNoteBook backend listening at http://localhost:${port}`)
 })
