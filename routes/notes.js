a679248db89c95d48a7bdaa4ced5b44b3a2e249f
VMNthemaster <varad.nadkarni09@gmail.com>
2022-01-19 23:57:03 +0530

    hi

--- routes/notes.js
+++ routes/notes.js
@@ -9,7 +9,10 @@ const { findByIdAndUpdate, findOne, findById } = require('../models/Note');
 //ROUTE 1: get all the notes details using get "/api/notes/getuser". Login required
 router.get('/fetchallnotes', async (req, res) => {
     try {
-        const notes = await Note.find({ user: req.user.id });
+        // const notes = await Note.find({ user: req.user.id });
+        const notes = {
+            a: "hi"
+        }
         res.json(notes);
     } catch (error) {
         console.error(error.message);
